b1a433437f4326ae76bdca44d57024926c2ae8a5
Ómar Kjartan Yasin <omarkj@gmail.com>
2011-06-30 12:31:11 +0000

    day counter

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 		      var today = new Date();
 		      var timeout = new Date(2011,6,9);
 		      var diff = timeout - today;
-		      var days = Math.round(diff / 86400000);
+		      var days = Math.round(diff / 86400000) + 1;
 		      if (days >= 0) {
 			  $("div#remainingDays span").html(days);
 		      } else {
