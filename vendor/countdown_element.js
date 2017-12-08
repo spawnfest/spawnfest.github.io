42eb41b07067a46e1512c7bc8e9e1d2f1311189e
Marcos Almonacid <m.almonacid@pegb.tech>
2017-12-09 01:05:32 +0400

    remove 'days' from countdown. add small countdown for mobile

--- vendor/countdown_element.js
+++ vendor/countdown_element.js
@@ -1,4 +1,4 @@
-var Countdown = new Countdown({
+var BigCountdown = new Countdown({
   year     : 2017,
   month    : 12,
   day      : 11,
@@ -6,10 +6,43 @@ var Countdown = new Countdown({
   minute   : 0,
   second   : 0,
   timezone : 0,
-  rangeHi  : "day",
+  rangeHi  : "hour",
   target   : "countdown",
   interval : 1000,
   width    : 500,
+  height   : 100,
+  hideLine : 1,
+  numbers  : {
+    font  : "Arial",
+    color : "#FFFFFF",
+    bkgd  : "#ff5e00",
+    rounded : 0.15,
+    shadow  : {
+      x : 0,
+      y : 3,
+      s : 4,
+      c : "#000000",
+      a : 0.4
+    }
+  },
+  labels: {
+    color  : "#c1bbbb",
+    weight : "normal"
+  }
+});
+
+var SmallCountdown = new Countdown({
+  year     : 2017,
+  month    : 12,
+  day      : 11,
+  hour     : 0,
+  minute   : 0,
+  second   : 0,
+  timezone : 0,
+  rangeHi  : "hour",
+  target   : "countdown-small",
+  interval : 1000,
+  width    : 350,
   height   : 100,
   hideLine : 1,
   numbers  : {
