9de58a4f39bd3e6939ea36b1bfc7d3e52630720c
Brujo Benavides <fernando.benavides@adroll.com>
2018-09-04 08:38:44 -0300

    Revert "Merge pull request #21 from spawnfest/amilkr.event_starts"
    This reverts commit 17b98db997bce84ad4999b93c2c34c5ba8bdbb61, reversing
    changes made to 4f83ae0aa5716ffa086de3e3e2a0f67cf1d49ee0.

--- vendor/countdown_element.js
+++ vendor/countdown_element.js
@@ -1,12 +1,12 @@
-var BigCountdown = new Countdown({
+var Countdown = new Countdown({
   year     : 2017,
   month    : 12,
-  day      : 11,
+  day      : 9,
   hour     : 0,
   minute   : 0,
   second   : 0,
   timezone : 0,
-  rangeHi  : "hour",
+  rangeHi  : "day",
   target   : "countdown",
   interval : 1000,
   width    : 500,
@@ -15,40 +15,7 @@ var BigCountdown = new Countdown({
   numbers  : {
     font  : "Arial",
     color : "#FFFFFF",
-    bkgd  : "#ff5e00",
-    rounded : 0.15,
-    shadow  : {
-      x : 0,
-      y : 3,
-      s : 4,
-      c : "#000000",
-      a : 0.4
-    }
-  },
-  labels: {
-    color  : "#c1bbbb",
-    weight : "normal"
-  }
-});
-
-var SmallCountdown = new Countdown({
-  year     : 2017,
-  month    : 12,
-  day      : 11,
-  hour     : 0,
-  minute   : 0,
-  second   : 0,
-  timezone : 0,
-  rangeHi  : "hour",
-  target   : "countdown-small",
-  interval : 1000,
-  width    : 350,
-  height   : 100,
-  hideLine : 1,
-  numbers  : {
-    font  : "Arial",
-    color : "#FFFFFF",
-    bkgd  : "#ff5e00",
+    bkgd  : "#1abf17",
     rounded : 0.15,
     shadow  : {
       x : 0,
