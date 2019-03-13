a54af6d8b38fcc354028319a93b8473af0a17b81
Brujo Benavides <fernando.benavides@adroll.com>
2019-03-13 07:50:03 -0300

    Move 2018 to 2019

--- vendor/countdown_element.js
+++ vendor/countdown_element.js
@@ -1,9 +1,9 @@
 var Countdown = new Countdown({
-  year     : 2018,
-  month    : 11,
-  day      : 26,
+  year     : 2019,
+  month    : 9,
+  day      : 23,
   hour     : 0,
-  minute   : 30,
+  minute   : 0,
   second   : 0,
   timezone : 0,
   rangeHi  : "day",
