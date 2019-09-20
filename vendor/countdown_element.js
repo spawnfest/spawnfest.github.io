006206c2127fa4f2e5f6a7f82929f60a4e11591d
Marcos Almonacid <m.almonacid@pegb.tech>
2019-09-20 20:23:31 +0400

    countdown - start event

--- vendor/countdown_element.js
+++ vendor/countdown_element.js
@@ -1,7 +1,7 @@
 var Countdown = new Countdown({
   year     : 2019,
   month    : 9,
-  day      : 23,
+  day      : 21,
   hour     : 0,
   minute   : 0,
   second   : 0,
@@ -15,7 +15,7 @@ var Countdown = new Countdown({
   numbers  : {
     font  : "Arial",
     color : "#FFFFFF",
-    bkgd  : "#ff4500",
+    bkgd  : "#04458c",
     rounded : 0.15,
     shadow  : {
       x : 0,
